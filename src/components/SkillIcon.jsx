1d1137a35da3e636db95082078bc1c597c1c566b
Priyanshu Uchat <85685604+ITSMEPRIYANSHUUCHAT@users.noreply.github.com>
2024-07-01 06:49:57 +0530

    fixing things in SkillIcon.jsx

--- src/components/SkillIcon.jsx
+++ src/components/SkillIcon.jsx
@@ -2,8 +2,9 @@ import React from "react";
 
 const SkillIcon = ({ path }) => {
  return (
- <div className="flex items-center justify-center w-[80px] md:w-[90px] 2xl:w-[110px] h-[80px] md:h-[90px] 2xl:h-[110px] rectangle rounded-lg bg-[#292929]">
- <img src={path} className="w-[74px] md:w-[10px] 2xl:w-[200px] h-[60px]" />
+ <div className="flex items-center justify-center w-[80px] md:w-[90px] 
+ 2xl:w-[110px] h-[80px] md:h-[90px] 2xl:h-[110px] rectangle rounded-lg bg-[#292929]">
+ <img src={path} className="w-[74px] md:w-[40px] 2xl:w-[100px] h-[60px]" />
  </div>
  );
 };
